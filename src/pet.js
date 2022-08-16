b44ff39e5a07d089180814e1ff122090e43f632d
ELIAS-BARAKA <dbarakaelias@outlook.com>
2022-08-16 11:20:07 +0300

    done with growUp function

--- src/pet.js
+++ src/pet.js
@@ -2,12 +2,14 @@ function Pet(name, health, hunger, fitness, age) {
   this.name = name;
   this.health = 100;
   this.hunger = 0;
-  this.fitness = 100;
+  this.fitness = 10;
   this.age = 0;
 }
 
 Pet.prototype.growUp = function (){
     this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
 };
 
 module.exports = Pet;
--- test/pet.test.js
+++ test/pet.test.js
@@ -1,5 +1,7 @@
 const Pet = require("../src/pet");
 
+
+
 describe("constructor", () => {
   it("returns an object", () => {
     const pet = new Pet("Fido");
@@ -9,9 +11,19 @@ describe("constructor", () => {
 describe("growUp", () => {
   it("increments the age by 1", () => {
     const pet = new Pet("Fido");
-
     pet.growUp();
-
     expect(pet.age).toEqual(1);
+  });
+
+  it("increase hunger by 5", () => {
+    const pet = new Pet("Fido");
+    pet.growUp();
+    expect(pet.hunger).toBe(5);
+  });
+
+  it("decrease fitness by 3", () => {
+    const pet = new Pet("Fido");
+    pet.growUp();
+    expect(pet.fitness).toBe(7);
   });
 });
